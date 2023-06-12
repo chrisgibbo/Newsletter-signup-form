f729f227a9fdf2949f7ad3c821f44e29722176bb
Chris Gibbons <christ487@icloud.com>
2023-06-12 22:21:10 +0100

    removed JS file

--- index.js
+++
@@ -1,8 +0,0 @@
-// const emailInput = document.getElementById('email');
-// const submitBtn = document.getElementById('submitBtn');
-// const displayEmail = document.getElementById('displayEmail');
-
-// submitBtn.addEventListener("click", function() {
-//     const email = emailInput.value;
-//     displayEmail.textContent = email;
-// });
